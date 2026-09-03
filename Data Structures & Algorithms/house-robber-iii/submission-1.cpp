0769580b6284adc575f25a59aa0dca41657b39ee
Nipun Gupta <nipungupta082006@gmail.com>
2026-09-03 22:08:20 +0530

    Add: house-robber-iii - submission-1

---
+++ Data Structures & Algorithms/house-robber-iii/submission-1.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    unordered_map<TreeNode*, int> dp;
+public:
+    int solve(TreeNode* root) {
+        if(dp.count(root)) return dp[root];
+
+        int ans = root->val;
+        if(root->left) ans += solve(root->left->left) + solve(root->left->right);
+        if(root->right) ans += solve(root->right->left) + solve(root->right->right);
+
+        ans = max(ans, solve(root->left) + solve(root->right));
+        return dp[root] = ans;
+    }
+
+    int rob(TreeNode* root) {
+        dp[nullptr] = 0;
+
+        return solve(root);
+    }
+};
