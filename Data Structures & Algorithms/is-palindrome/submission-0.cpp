0407f5c4c93508bbe76ef938e27a7a581484a181
Nipun Gupta <nipungupta082006@gmail.com>
2026-09-05 20:08:19 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string t = "";
+        for(char c : s) {
+            if(isalnum(c)) t += tolower(c);
+        }
+
+        return t == string(t.rbegin(), t.rend());
+    }
+};
